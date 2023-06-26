b23458ad0b7ca90d434c97b93b91907a659569c6
phil <b00363747@essec.edu>
2023-06-26 22:23:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,37 +25,62 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
 
-previous_move = 'F'
-previous_score = 0
-consecutive_hits_count = 0
+service_url = None
+player_url = None
+player_x = None
+player_y = None
+player_direction = None
+player_score = None
+was_hit = None
+previous_score = None
 score_stagnant_count = 0
+consecutive_hits_count = 0
 
-# Global variables
-player = None
-opponents = []
+def set_player_and_opponents(data):
+    global player_url, player_x, player_y, player_direction, player_score, was_hit, previous_score
+    global score_stagnant_count, consecutive_hits_count
 
+    arena_data = data['arena']
+    player_data = None
+    opponents_data = []
 
-# Helper function to calculate Euclidean distance between two points
+    # Find player's data based on self href URL
+    for url, info in arena_data['state'].items():
+        if info['_links']['self']['href'] == player_url:
+            player_data = info
+        else:
+            opponents_data.append(info)
+
+    # Set player's attributes
+    player_x = player_data['x']
+    player_y = player_data['y']
+    player_direction = player_data['direction']
+    player_score = player_data['score']
+    was_hit = player_data['wasHit']
+    previous_score = player_score
+
+    # Reset counts
+    score_stagnant_count = 0
+    consecutive_hits_count = 0
+
+    return opponents_data
+
 def calculate_distance(x1, y1, x2, y2):
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-# Helper function to calculate threat level of an opponent
 def calculate_threat_level(opponent, player_x, player_y):
-    opp_x, opp_y = opponent['position']
+    opp_x, opp_y = opponent['x'], opponent['y']
     distance = calculate_distance(player_x, player_y, opp_x, opp_y)
     return 1 / distance
 
-# Helper function to get the direction of the opponent
 def get_opponent_direction(player_x, player_y, opponents):
     for opponent in opponents:
-        opp_x, opp_y = opponent['position']
+        opp_x, opp_y = opponent['x'], opponent['y']
         if calculate_distance(player_x, player_y, opp_x, opp_y) <= 3:
             return opponent['direction']
 
-    # Default to a random direction if unable to determine the opponent's direction
     return random.choice(['N', 'S', 'W', 'E'])
 
-# Helper function to check if any opponent is in front and within range distance 3
 def is_any_opponent_in_front(player_x, player_y, player_direction, opponents):
     directions = {
         'N': (0, -1),
@@ -67,24 +92,12 @@ def is_any_opponent_in_front(player_x, player_y, player_direction, opponents):
 
     dx, dy = directions[player_direction]
     for opponent in opponents:
-        opp_x, opp_y = opponent['position']
+        opp_x, opp_y = opponent['x'], opponent['y']
         if (opp_x - player_x) * dx >= 0 and (opp_y - player_y) * dy >= 0:
             if calculate_distance(player_x, player_y, opp_x, opp_y) <= range_distance:
                 return True
 
     return False
-
-def set_players_and_opponents(data):
-    global players, opponents
-
-    players_data = data['state']
-    self_href = data['_links']['self']['href']
-
-    for player_url, player_data in players_data.items():
-        if player_url == self_href:
-            players.append(player_data)
-        else:
-            opponents.append(player_data)
 
 @app.route("/", methods=['GET'])
 def index():
@@ -94,30 +107,18 @@ def index():
 def move():
     #Original
     #request.get_data()
-    global player, opponents
-    data = request.get_json()
+global player_score, previous_score, score_stagnant_count, consecutive_hits_count
 
-    # Set player and opponents
-    set_player_and_opponent(data)
+    opponents_data = set_player_and_opponents(request.json)
 
-    # Extract player information
-    player_x = player['position'][0]
-    player_y = player['position'][1]
-    player_direction = player['direction']
-    was_hit = player['wasHit']
-    player_score = player['score']
-    
-    # Check if score is increasing
     if player_score > previous_score:
         previous_score = player_score
         score_stagnant_count = 0
-    # Check if consecutive hits occurred and move to escape
+
     if was_hit:
         consecutive_hits_count += 1
-        if consecutive_hits_count == 2:
-            # Get the last hit direction
-            last_hit_direction = get_opponent_direction(player_x, player_y, opponents)
-            # Move to escape based on the last hit direction
+        if consecutive_hits_count >= 2:
+            last_hit_direction = get_opponent_direction(player_x, player_y, opponents_data)
             if last_hit_direction != player_direction:
                 if last_hit_direction == 'N':
                     return 'R'
@@ -125,7 +126,17 @@ def move():
                     return 'L'
                 else:
                     return 'T'
-            return 'F'
+
+    if not was_hit:
+        consecutive_hits_count = 0
+
+    if is_any_opponent_in_front(player_x, player_y, player_direction, opponents_data):
+        return 'T'
+
+    if player_score == previous_score and score_stagnant_count <= 4:
+        score_stagnant_count += 1
+        return random.choices(['F', 'R'], weights=[0.7, 0.3])[0]
+
             
     # Original
     return moves[random.randrange(len(moves))]
